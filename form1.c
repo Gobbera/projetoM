de5423cca3ae952d5b4753428862be7fcd9cc08a
Gobbera <103601180+Gobbera@users.noreply.github.com>
2023-02-27 14:20:31 -0300

    Update form1.c

--- form1.c
+++ form1.c
@@ -32,10 +32,36 @@ void imprimir (No *no) {
         imprimir(no->proximo);
     }
 }
-int main () {
-    int op, valor;
+void startGame() {
     char jogador1[5] [50];
     char jogador2[5] [50];
+    printf("digite o nome do jogador 1 : ");
+    scanf("%s", &jogador1);
+    printf("digite o nome do jogador 2 : ");
+    scanf("%s", &jogador2);
+    printf(" \n %s\n ", jogador1);
+    printf("------VS------");
+    printf(" \n %s\n ", jogador2);
+    play(jogador1, jogador2);
+}
+
+void play(jogador1, jogador2) {
+    //playerTurn *turn = jogador1;
+    int num1;
+    printf("Vez de %s \n", jogador1);
+    do {
+    printf("Escolha um numero \n");
+    scanf("%d", &num1);
+    printf("%s jogou: %d \n", jogador1, num1);
+    if(num1 > 5) {
+        printf("Jogada invalida, jogue outro numero \n");
+    }
+    }while(num1 > 5);
+    return 0;
+}
+
+int main () {
+    int op, valor;
 
     No *no; Pilha p; p.tam = 0; p.topo = NULL;
     printf("Jogo ProjetoM");
@@ -47,24 +73,10 @@ int main () {
             printf("Saindo...\n");
             break;
         case 1:
-            printf("digite o nome do jogador 1 : ");
-            scanf("%s", & jogador1);
-            printf("digite o nome do jogador 2 : ");
-            scanf("%s", &jogador2);
-            printf(" \n %s\n ", jogador1);
-            printf("------VS------");
-            printf(" \n %s\n ", jogador2);
-            play(jogador1, jogador2);
+            startGame();
             break;
         }
     }while(op != 0);
     return 0;
-    
-}
-int play(jogador1, jogador2) {
-    int num1;
-    printf("Vez de %s \n", jogador1);
-    printf("Escolha um numero \n");
-    scanf("%d", num1);
-    printf("jogou: %d \n", num1);
+
 }
