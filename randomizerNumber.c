8f8d442b94bd830cbb50b642b3094a8850d0f7dd
Gobbera <103601180+Gobbera@users.noreply.github.com>
2023-03-03 20:19:23 -0300

    Update randomizerNumber.c

--- randomizerNumber.c
+++ randomizerNumber.c
@@ -5,25 +5,29 @@
 int main() {
     randomizerNumber();
 
+
 }
 void randomizerNumber(){
-	int lower = 10, upper = 20, count = 1;
+	int lower = 10, upper = 80, count = 1;
 	srand(time(0));
 	int i;
 	for (i = 0; i < count; i++) {
-		int lowerNum = (rand() %
+		int randomNum = (rand() %
 		(upper - lower + 1)) + lower;
-		printf("valor inicial: %d \n", lowerNum);
-	randomizerNumber2(lowerNum);
+		printf("valor inicial: %d \n", randomNum);
+		range(randomNum);
 	}
+}
+void range(randomNum) {
+    int lower = 10, upper = 20, count = 1;
+	srand(time(0));
+	int i;
+	for (i = 0; i < count; i++) {
+		int range = (rand() %
+		(upper - lower + 1)) + lower;
+		printf("valor da range: %d \n", range);
+		int soma = range + randomNum;
+		printf("valor a bater: %d \n", soma);
+	}
+}
 
-}
-randomizerNumber2(lowerNum) {
-    int lowerNum, upper = 100, count = 1;
-    int i;
-    for (i = 0; i < count; i++) {
-		int upperNum = (rand() %
-		(upper - lower + 1)) + lower;
-		printf("valor a bater: %d \n", upperNum);
-	}
-}
